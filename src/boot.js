a5d413b550fd47627861444a28a180e244300ee9
Babar Javaid <babar.javaid@blueeast.com>
2018-05-23 08:08:50 +0500

    Added Apollo Engine

--- src/boot.js
+++ src/boot.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const Engine = require('apollo-engine').Engine;
 const graphql = require('graphql-server-express');
 const bodyParser = require('body-parser');
 const { getSchema } = require('./schema/index');
@@ -9,6 +10,7 @@ const startSubscriptionServer = require('./subscriptions');
 const patchChangeStream = require('./subscriptions/patchChangeStream');
 
 module.exports = function(app, options) {
+  console.log('Option Param', options);
   const models = app.models();
   
   _.forEach(models, (model) =>  {
@@ -16,16 +18,49 @@ module.exports = function(app, options) {
   });
 
   const schema = getSchema(models, options);
-
+  // const apollo = options.apollo;
+  const apollo = {
+    apiKey: 'GPjWuVoaPBzEDA9RvGzP9A',
+    debugLevel: 'DEBUG'
+  }
   const graphiqlPath = options.graphiqlPath || '/graphiql';
   const path = options.path || '/graphql';
+
+    if (apollo) {
+      if (!apollo.apiKey) {
+        // apollo.apiKey = 'GPjWuVoaPBzEDA9RvGzP9A';
+        throw new Error('Apollo engine api key is not defined');
+      }
+      const engine = new Engine({
+        engineConfig: {
+          apiKey: apollo.apiKey,
+          logging: {
+            level: apollo.debugLevel || 'DEBUG', // Engine Proxy
+            // logging level.
+            // DEBUG, INFO, WARN
+            // or ERROR
+          },
+        },
+        graphqlPort: apollo.graphqlPort || 2000, // GraphQL port
+        endpoint: path || '/graphql', // GraphQL endpoint suffix -
+        // '/graphql' by default
+        dumpTraffic: true, // Debug configuration that logs traffic between
+        // Proxy and GraphQL server
+      });
+  
+      engine.start();
+  
+      app.use(engine.expressMiddleware());
+    }
 
   app.use(path, bodyParser.json(), graphql.graphqlExpress(req => ({
     schema,
     context: {
       app,
       req
-    }
+    },
+    tracing: false,
+    cacheControl: true
   })));
 
   app.use(graphiqlPath, graphql.graphiqlExpress({
