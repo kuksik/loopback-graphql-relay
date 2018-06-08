7358ffb0335931164967f5e086ee874c3af48851
Abdul Ghaffar <abdul.ghaffar@blueeast.com>
2018-06-08 22:06:32 +0500

    pubsub updated

--- src/subscriptions/subscriptionWithPayload.js
+++ src/subscriptions/subscriptionWithPayload.js
@@ -1,6 +1,6 @@
 
 const _ = require('lodash');
-const { LoopbackPubSub } = require('graphql-loopback-subscriptions');
+const { PubSub } = require('graphql-loopback-subscriptions');
 const { withFilter } = require('graphql-subscriptions');
 const { getType } = require('../types/type');
 
@@ -10,7 +10,7 @@ const {
   GraphQLObjectType,
 } = require('graphql');
 
-const loopbackPubSub = new LoopbackPubSub();
+const loopbackPubSub = new PubSub();
 
 function resolveMaybeThunk(maybeThunk) {
   return typeof maybeThunk === 'function' ? maybeThunk() : maybeThunk;
@@ -82,6 +82,24 @@ module.exports = function
       },
       (payload, variables, context, info) => {
         return true;
-      }),
+        // return (payload.object.data.__data.id.toString() === variables.input.options.id);
+
+        // const subscriptionPayload = {
+        //   clientSubscriptionId: variables.input.clientSubscriptionId,
+        //   remove: variables.input.remove,
+        //   create: variables.input.create,
+        //   update: variables.input.update,
+        //   opts: variables.input.options,
+        // };
+        //
+        // subscriptionPayload.model = model;
+        //
+        // try {
+        //   loopbackPubSub.subscribe(info.fieldName, null, subscriptionPayload);
+        // } catch (ex) {
+        //   console.log(ex);
+        // }
+      },
+    )
   };
 };
