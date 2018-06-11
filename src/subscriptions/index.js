4b6bfe7b5d4ede34dceacbcd4fa3d17cd93a4fed
Abdul Ghaffar <abdul.ghaffar@blueeast.com>
2018-06-11 17:51:06 +0500

    SSL enabled

--- src/subscriptions/index.js
+++ src/subscriptions/index.js
@@ -6,123 +6,73 @@ const { execute, subscribe } = require('graphql');
 const { SubscriptionServer } = require('subscriptions-transport-ws');
 
 const { createServer } = require('http');
-
-// const { schema } = require('./schema');
+const https = require('https');
+const fs = require('fs');
 
 module.exports = function (app, schema, opts) {
+  const subscriptionOpts = opts.subscriptionServer || {};
+  const WS_PORT = subscriptionOpts.port || 5000;
 
-  const PORT = 3000;
-  const server = app;
+  const validateToken = authToken => new Promise((resolve, reject) => {
+    let accessToken = '';
+    if (subscriptionOpts.AccessTokenModel) {
+      accessToken = app.models[subscriptionOpts.AccessTokenModel];
+    } else { accessToken = app.models.AccessToken; }
 
-  server.use('/graphql', bodyParser.json(), graphqlExpress({
-    schema
+    accessToken.resolve(authToken, (err, token) => {
+      if (token) {
+        resolve();
+      } else reject();
+    });
+  });
+
+  function wsConnect(connectionParams) {
+    if (subscriptionOpts.auth && connectionParams.authToken) {
+      return validateToken(connectionParams.authToken).then(() => true).catch(() => false);
+    } else if (!subscriptionOpts.auth) return true;
+    return false;
+  }
+
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema,
   }));
-  if (!opts.subscriptionServer.graphiqlHost) opts.subscriptionServer.graphiqlHost = 'localhost';
-  server.use('/graphiql', graphiqlExpress({
+
+  if (!subscriptionOpts.graphiqlHost) subscriptionOpts.graphiqlHost = 'localhost';
+  if (subscriptionOpts.ssl) subscriptionOpts.wsEndpointURL = `wss://${subscriptionOpts.graphiqlHost}:${WS_PORT}/subscriptions`;
+  else subscriptionOpts.wsEndpointURL = `ws://${subscriptionOpts.graphiqlHost}:${WS_PORT}/subscriptions`;
+
+  app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
-    subscriptionsEndpoint: `ws://${opts.subscriptionServer.graphiqlHost}:${PORT}/subscriptions`
+    subscriptionsEndpoint: subscriptionOpts.wsEndpointURL,
   }));
 
-// Wrap the Express server
-  const ws = createServer(server);
-  ws.listen(PORT, () => {
-    console.log(`Apollo Server is now running on http://localhost:${PORT}`);
+  let websocketServer = '';
+  if (subscriptionOpts.ssl) {
+    const ssl = {
+      key: fs.readFileSync(subscriptionOpts.keyPath),
+      cert: fs.readFileSync(subscriptionOpts.certPath),
+    };
+    websocketServer = https.createServer(ssl, (request, response) => {
+      response.writeHead(404);
+      response.end();
+    });
+  } else {
+    websocketServer = createServer(app);
+  }
+
+  websocketServer.listen(WS_PORT, () => {
+    console.log(`Apollo Server is now running on http://localhost:${WS_PORT}`);
     // Set up the WebSocket for handling GraphQL subscriptions
     new SubscriptionServer({
       execute,
       subscribe,
-      schema
+      schema,
+      onConnect: wsConnect,
     }, {
-      server: ws,
+      server: websocketServer,
       path: '/subscriptions',
     });
   });
-  return ws;
-//
-// const { SubscriptionServer } = require('subscriptions-transport-ws');
-// const { execute, subscribe } = require('graphql');
-// const bodyParser = require('body-parser');
-// const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-//
-//
-//   const PORT = 3000;
-//
-//   app.use('/graphiql', graphiqlExpress({
-//     endpointURL: '/graphql',
-//     subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`,
-//   }));
-//
-//   app.use('/graphql', bodyParser.json(), graphqlExpress(req => ({
-//     schema,
-//     rootValue: global,
-//     graphiql: false,
-//     context: {
-//       app,
-//       req,
-//     },
-//   })));
-//
-//   const server = createServer(app);
-//   server.listen(PORT, () => {
-//     SubscriptionServer.create(
-//       { execute, subscribe, schema },
-//       { server, path: '/subscriptions' },
-//     );
-//     console.log(`GraphQL server running on port ${PORT}.`);
-//   });
-//
-//   // const subscriptionOpts = opts.subscriptionServer || {};
-//   //
-//   // const disable = subscriptionOpts.disable || false;
-//   //
-//   // if (disable === true) {
-//   //   return undefined;
-//   // }
-//   //
-//   // const WS_PORT = subscriptionOpts.port || 5000;
-//   // // const options = subscriptionOpts.options || {};
-//   // // const socketOptions = subscriptionOpts.socketOptions || {};
-//   //
-//   // let websocketServer = '';
-//   // if (subscriptionOpts.ssl) {
-//   //   const ssl = {
-//   //     key: fs.readFileSync(subscriptionOpts.keyPath),
-//   //     cert: fs.readFileSync(subscriptionOpts.certPath),
-//   //   };
-//   //   websocketServer = https.createServer(ssl, (request, response) => {
-//   //     response.writeHead(404);
-//   //     response.end();
-//   //   });
-//   // } else {
-//   //   websocketServer = createServer((request, response) => {
-//   //     response.writeHead(404);
-//   //     response.end();
-//   //   });
-//   // }
-//   //
-//   // websocketServer.listen(WS_PORT, () => console.log(`Websocket Server is now running on http(s)://localhost:${WS_PORT}`));
-//   //
-//   // const validateToken = authToken => new Promise((resolve, reject) => {
-//   //   let accessToken = '';
-//   //   if (subscriptionOpts.AccessTokenModel) { accessToken = app.models[subscriptionOpts.AccessTokenModel]; } else { accessToken = app.models.AccessToken; }
-//   //
-//   //   accessToken.resolve(authToken, (err, token) => {
-//   //     if (token) {
-//   //       resolve();
-//   //     } else reject();
-//   //   });
-//   // });
-//   //
-//   // function wsConnect(connectionParams) {
-//   //   if (subscriptionOpts.auth && connectionParams.authToken) {
-//   //     return validateToken(connectionParams.authToken).then(() => true).catch(() => false);
-//   //   } else if (!subscriptionOpts.auth) return true;
-//   //   return false;
-//   // }
-//   //
-//   // SubscriptionServer.create({
-//   //   schema, execute, subscribe, onConnect: wsConnect,
-//   // }, { server: websocketServer, path: '/' });
-//
-//   return server;
+
+  return websocketServer;
 };
