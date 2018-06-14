7cb8983b19ebdd7959575c38580f884b741a8a83
Abdul Ghaffar <abdul.ghaffar@blueeast.com>
2018-06-14 16:02:45 +0500

    unsub fixed

--- src/subscriptions/subscriptionWithPayload.js
+++ src/subscriptions/subscriptionWithPayload.js
@@ -1,6 +1,6 @@
 
 const _ = require('lodash');
-const { PubSub } = require('graphql-loopback-subscriptions');
+const { MQTTPubSub } = require('graphql-mqtt-subscriptions');
 const { withFilter } = require('graphql-subscriptions');
 const { getType } = require('../types/type');
 
@@ -10,7 +10,7 @@ const {
   GraphQLObjectType,
 } = require('graphql');
 
-const loopbackPubSub = new PubSub();
+const loopbackPubSub = new MQTTPubSub();
 
 function resolveMaybeThunk(maybeThunk) {
   return typeof maybeThunk === 'function' ? maybeThunk() : maybeThunk;
@@ -71,35 +71,37 @@ module.exports = function
         target = (payload) ? payload.object.target : null;
       }
 
-      return Promise.resolve(subscribeAndGetPayload(payload, args, context, info))
-        .then(resPayload => ({
+      return Promise.resolve(subscribeAndGetPayload(payload, args, context, info)).then(resPayload => (
+        {
           clientSubscriptionId, where, type, target, object: resPayload.data,
-        }));
+        }
+      ));
     },
     subscribe: withFilter(
       (payload, variables, context, info) => {
         return loopbackPubSub.asyncIterator(model, variables.input);
       },
-      (payload, variables, context, info) => {
-        return (variables.input.options.subId === payload.subscriptionId);
-        // return (payload.object.data.__data.id.toString() === variables.input.options.id);
-
-        // const subscriptionPayload = {
-        //   clientSubscriptionId: variables.input.clientSubscriptionId,
-        //   remove: variables.input.remove,
-        //   create: variables.input.create,
-        //   update: variables.input.update,
-        //   opts: variables.input.options,
-        // };
-        //
-        // subscriptionPayload.model = model;
-        //
-        // try {
-        //   loopbackPubSub.subscribe(info.fieldName, null, subscriptionPayload);
-        // } catch (ex) {
-        //   console.log(ex);
-        // }
-      },
-    )
+      // (payload, variables, context, info) => {
+      //
+      //   return true;
+      //   // return (variables.input.options.subId === payload.subscriptionId);
+      //
+      //   // const subscriptionPayload = {
+      //   //   clientSubscriptionId: variables.input.clientSubscriptionId,
+      //   //   remove: variables.input.remove,
+      //   //   create: variables.input.create,
+      //   //   update: variables.input.update,
+      //   //   opts: variables.input.options,
+      //   // };
+      //   //
+      //   // subscriptionPayload.model = model;
+      //   //
+      //   // try {
+      //   //   loopbackPubSub.subscribe(info.fieldName, null, subscriptionPayload);
+      //   // } catch (ex) {
+      //   //   console.log(ex);
+      //   // }
+      // },
+    ),
   };
 };
